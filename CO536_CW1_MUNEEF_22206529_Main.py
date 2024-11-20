f4331e1acb009bcd50490fbece75df91e54ec344
Muneef Mumthas <muneefmumthas1431@.com>
2024-11-20 02:22:35 +0000

    Finnally added the Minimax Ai, after so many tries/debugging its 2.20 am T_T

--- CO536_CW1_MUNEEF_22206529_Main.py
+++ CO536_CW1_MUNEEF_22206529_Main.py
@@ -3,6 +3,7 @@ import pygame
 import numpy as np
 import time
 import random
+import copy
 
 #initialising the variables
 Width = 900
@@ -199,9 +200,9 @@ class Board:
 
 #AI class
 class AI:
-    def __init__(self, difficulty="easy", player=2):
-        self.difficulty = difficulty
-        self.player = player
+    def __init__(self):
+        self.difficulty = "easy"
+        self.player = 2
 
         #Random AI - Method to select a random empty square
     def random_ai(self, board):
@@ -212,33 +213,70 @@ class AI:
     
         #Minimax AI - Method to select the best move based on the minimax algorithm
     def minimax(self, board, maximizing):
-
+        #Returns the evaluation and the best move
+        
         #Base case to check the final state of the game
         case = board.check_winner()
 
         # AI (player 2) wins
         if case == 2:
-            return 1
+            return 1, None #
         
-        # Human (player 1) wins
+        # player 1 wins
         elif case == 1:
-            return -1
+            return -1, None
         
         # Draw
         elif board.is_board_full():
-            return 0
+            return 0, None
         
-        
+        #Maximizing player (AI)
+        if maximizing:
+            max_eval = -1000
+            best_move = None
+            empty_squares = board.get_empty_squares()
+
+            for (row, col) in empty_squares:
+                temp_board = copy.deepcopy(board)
+                temp_board.mark_square(row, col, self.player)
+                eval = self.minimax(temp_board, False)[0]
+
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = (row, col)
+                
+            return max_eval, best_move
+
+        #Minimizing player - player 1
+        elif not maximizing:
+            min_eval = 1000
+            best_move = None
+            empty_squares = board.get_empty_squares()
+
+            for (row, col) in empty_squares:
+                temp_board = copy.deepcopy(board)
+                temp_board.mark_square(row, col, 1)
+                eval = self.minimax(temp_board, True)[0]
+
+                if eval < min_eval:
+                    min_eval = eval
+                    best_move = (row, col)
+            
+            return min_eval, best_move
 
     def evaluate(self, main_board):
 
         #Easy Mode - Random Ai
         if self.difficulty == "easy":
+            eval = "Random"
             move = self.random_ai(main_board)
 
         elif self.difficulty == "hard":
-            self.minimax(main_board, False)
-            
+            eval, move = self.minimax(main_board, True)
+        
+        #printing the move and evaluation for debugging
+        print(f"AI chose to mark square {move} with evaluation {eval}")
+  
         return move
     
         
@@ -383,9 +421,15 @@ def main():
                             screen.fill(bg_colour)
                             game.game_mode = "ai_game"
                             game.restart()
+                            game.ai.difficulty = "easy"
 
                         elif hard_button.collidepoint(event.pos):    
-                            print("Hard")
+                            click_sound.play()
+                            current_screen = "game"
+                            screen.fill(bg_colour)
+                            game.game_mode = "ai_game"
+                            game.restart()
+                            game.ai.difficulty = "hard"
 
                         elif back_button_difficulty.collidepoint(event.pos):
                             click2_sound.play()
@@ -479,7 +523,7 @@ def main():
 
         #AI's turn in ai mode
         elif current_screen == "game" and game.game_mode == "ai_game" and game.player == game.ai.player:
-            
+            pygame.display.update()
             row, col = game.ai.evaluate(game.board)
 
             game.board.mark_square(row, col, game.ai.player)
