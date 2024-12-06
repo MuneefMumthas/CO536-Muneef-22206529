833181a16afbe88ce1f1669a90ba57dca00c1717
Muneef Mumthas <muneefmumthas1431@.com>
2024-12-06 11:48:50 +0000

    did some final checks for the code

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # Game Title: Tic Tac Toe
 
 # config.py - This file is used to store the current theme of the game 
-# and to change it by changing the current_theme variable to either "blue", "purple" or "red".
+# and to change it by changing the current_theme variable to either "blue", "purple" or "red" from other classes/files.
 
 
 current_theme = "blue"
